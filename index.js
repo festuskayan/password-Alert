5c2d462ed0b8129eff26181d439e350ec870dabf
festuskayan <kayanfestus@gmail>
2023-08-17 23:56:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// var x=10 
+// var y=10
+// if (x<y){
+//     console.log("x is less than y")
+// }
+// else if(x>y){
+//     console.log("x is greater than y")
+// }
+// else{console.log("x equals y")}\
+// var openeye=document.getElementById("shine")
+// var firstinput=document.getElementById("password")
+function passwordVisibility(){
+    if(document.getElementById("password"))
+}
